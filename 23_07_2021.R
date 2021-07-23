c0e5d4b01d9db241779c5f098095dceccc381e95
rladieslavras <50925312+rladieslavras@users.noreply.github.com>
2021-07-23 16:19:30 -0300

    Add files via upload
    Pacote batata

---
+++ 23_07_2021.R
@@ -0,0 +1,19 @@
+# Dica de Sexta RLadies Lavras (15/07/2021)
+
+install.packages("batata")
+library(batata)
+
+#Funções
+
+fresh_start()
+globals() 
+install_starred_cran() #
+install_most_starred()
+rm_latest_packages()
+rm_since_packages()
+rm_today_packages()
+rm_yesterday_packages()
+since_packages()
+today_packages()
+yesterday_packages()
+latest_packages()
